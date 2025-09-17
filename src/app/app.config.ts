e66e0154ffaabeaa5240b14668860832d788fdc1
Cole Williams <wcole2222@gmail.com>
2025-09-17 17:27:14 -0600

    install ngrx/effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideStore()
+    provideStore(),
+    provideEffects()
 ]
 };
